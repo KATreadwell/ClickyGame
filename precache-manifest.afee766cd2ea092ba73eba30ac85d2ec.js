1dfb15cd7731363b8dd223d41e8842ece195e2b5
KAT <katreadwell1@gmail.com>
2020-06-21 22:16:09 -0700

    Updates

---
+++ precache-manifest.afee766cd2ea092ba73eba30ac85d2ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d363a7c748e673b2ac6e4c1f286a5165",
+    "url": "/KATreadwell/ClickyGame/index.html"
+  },
+  {
+    "revision": "39067786318a7050ed61",
+    "url": "/KATreadwell/ClickyGame/static/css/main.f9acf145.chunk.css"
+  },
+  {
+    "revision": "77ec5a0aa6834a00ede5",
+    "url": "/KATreadwell/ClickyGame/static/js/2.69b5d891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KATreadwell/ClickyGame/static/js/2.69b5d891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39067786318a7050ed61",
+    "url": "/KATreadwell/ClickyGame/static/js/main.f46c3b77.chunk.js"
+  },
+  {
+    "revision": "3b1f5c2dea275b8282ef",
+    "url": "/KATreadwell/ClickyGame/static/js/runtime-main.71a1bcc3.js"
+  }
+]);
